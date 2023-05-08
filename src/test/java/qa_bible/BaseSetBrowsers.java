ed5eea863994b16a019d4dfda64746330d93fba8
viktor <lukashevviktor@gmail.com>
2023-05-08 12:55:45 +0300

    Почему не работает CHROME? И все равно не работает

--- src/test/java/qa_bible/BaseSetBrowsers.java
+++ src/test/java/qa_bible/BaseSetBrowsers.java
@@ -2,9 +2,14 @@ package qa_bible;
 
 import constant.URL;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -13,20 +18,23 @@ import java.util.concurrent.TimeUnit;
 
 
 public class BaseSetBrowsers {
+    // актуальный драйвер берем от сюда:
+    // https://chromedriver.chromium.org/downloads
 
     protected WebDriver driver;
     private static final String YANDEX_BROWSER = "yandex";
     private static final String CHROME_BROWSER = "chrome";
     private static final String PATH_TO_YANDEX_DRIVER = "src/main/resources/yandex/chromedriver.exe";
+    private static final String PATH_TO_CHROME_DRIVER = "src/main/resources/chrome/chromedriver.exe";
     private static final String BINARY_PATH = "C:/Users/lvikt/AppData/Local/Yandex/YandexBrowser/Application/browser.exe";
 
-    @BeforeMethod
+    @BeforeEach
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         String browser = System.getProperty("browser", CHROME_BROWSER);
         switch (browser) {
             case CHROME_BROWSER:
-                System.setProperty("webdriver.chrome.driver", null);
+                System.setProperty("webdriver.chrome.driver", PATH_TO_CHROME_DRIVER); // укажите путь до chromedriver
                 driver = new ChromeDriver();
                 break;
             case YANDEX_BROWSER:
@@ -44,7 +52,7 @@ public class BaseSetBrowsers {
         driver.get(URL.URL_BASE);
     }
 
-    @AfterMethod
+    @AfterEach
     public void tearDown() {
         driver.close();
         if (driver != null) {
